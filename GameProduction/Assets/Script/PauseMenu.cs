063c93a8d2f9c19f5bfbb9e285a7998657a2dc11
Helles <yelles97@gmail.com>
2019-01-11 03:19:03 +0800

    v0.26.1 (Resume Button)
    +Added Resume button

--- GameProduction/Assets/Script/PauseMenu.cs
+++ GameProduction/Assets/Script/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -30,5 +31,10 @@ public class PauseMenu : MonoBehaviour
         {
             isPaused = !isPaused;
         }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
     }
 }
