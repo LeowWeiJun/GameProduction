0b3a9c8cf9090338465622278ba093c587549d23
Helles <yelles97@gmail.com>
2019-01-11 00:12:42 +0800

    v0.25 (Exit Application)
    +Added Exit Button by double tap on back button

--- GameProduction/Assets/Script/MainMenu.cs
+++ GameProduction/Assets/Script/MainMenu.cs
@@ -5,20 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    int exitCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        exitCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            exitCount++;
+            if (!IsInvoking("disableDoubleClick"))
+                Invoke("disableDoubleClick", 0.3f);
+        }
+        if (exitCount == 2)
+        {
+            CancelInvoke("disableDoubleClick");
+            Application.Quit();
+        }
     }
 
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void disableDoubleClick()
+    {
+        exitCount = 0;
     }
 }
